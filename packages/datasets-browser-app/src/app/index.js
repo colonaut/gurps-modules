13a34d45467cbc12b80cb8d035bf22a69724f9a3
colonaut <colonaut@gmx.de>
2019-03-18 22:24:55 +0100

    make HOC for query

--- packages/datasets-browser-app/src/app/index.js
+++ packages/datasets-browser-app/src/app/index.js
@@ -6,6 +6,7 @@ import {ApolloProvider} from 'react-apollo';
 import {Query} from "react-apollo";
 import fetch from 'unfetch';
 import gql from "graphql-tag";
+import graphql from 'graphql-tag';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {List, ListItem} from 'material-ui/List';
@@ -30,21 +31,56 @@ const allAdvantages = gql`
     }
 `;
 
+const withAdvantagesQuery = (Component) =>
+class extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <Query query={allAdvantages}>
+            {({loading, error, data, fetchMore}) => {
+                if (loading) return "Loading...";
+                if (error) return `Error! ${error.message}`;
+
+                console.log(fetchMore)
+                return <Component data={data} fetchMore={fetchMore}/>;
+            }}
+        </Query>
+    };
+};
+
+
+const AdvantageList = ({data: {allAdvantages}}) => <List>
+    {allAdvantages.map((a, i) => <ListItem
+        key={`allAdvantages_${i}`}
+        primaryText={a.name}
+        secondaryText={`Cost: ${a.cost.join('/')} | Source: ${(a.source_books || ['-']).join(',')}`}
+    />)}
+</List>;
+
+
+const Foo = withAdvantagesQuery(AdvantageList);
 
 const App = () => <MuiThemeProvider>
+        <ApolloProvider client={client}>
+            <Foo/>
+        </ApolloProvider>
+</MuiThemeProvider>;
+
+
+const App_ = () => <MuiThemeProvider>
     <ApolloProvider client={client}>
         <Query query={allAdvantages}>
             {({loading, error, data}) => {
                 if (loading) return "Loading...";
                 if (error) return `Error! ${error.message}`;
 
-                //const random_index =  Math.floor(Math.random() * data.getQuotes.length);
-                //const quote = data.getQuotes[random_index];
                 const advantages = data.allAdvantages;
 
                 return (
                     <List>
-                        {advantages.map((a,i) => <ListItem
+                        {advantages.map((a, i) => <ListItem
                             key={`allAdvantages_${i}`}
                             primaryText={a.name}
                             secondaryText={`Cost: ${a.cost.join('/')} | Source: ${(a.source_books || ['-']).join(',')}`}
