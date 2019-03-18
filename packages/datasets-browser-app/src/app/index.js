769fcb317ebd085cfcb8d59b92f348c625dd2ee7
kalle <kalle.linsenboll@cleverbridge.com>
2019-03-18 12:54:58 +0100

    dataset-browser-app add material-ui to draft

--- packages/datasets-browser-app/src/app/index.js
+++ packages/datasets-browser-app/src/app/index.js
@@ -7,10 +7,14 @@ import {Query} from "react-apollo";
 import fetch from 'unfetch';
 import gql from "graphql-tag";
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {List, ListItem} from 'material-ui/List';
+
+
 const apollo_link = new HttpLink({ //create a HttpLink, this will prevent CORS prob.
-        uri: 'http://localhost:3000/graphql',
-        //fetch: fetch //TODO necc for testing (?) put it into test utils?
-    });
+    uri: 'http://localhost:3000/graphql',
+    //fetch: fetch //TODO necc for testing (?) put it into test utils?
+});
 const client = new ApolloClient({
     link: apollo_link,
     cache: new InMemoryCache()
@@ -27,26 +31,29 @@ const allAdvantages = gql`
 `;
 
 
-const App = () => <ApolloProvider client={client}>
-    <Query query={allAdvantages}>
-        {({loading, error, data}) => {
-            if (loading) return "Loading...";
-            if (error) return `Error! ${error.message}`;
+const App = () => <MuiThemeProvider>
+    <ApolloProvider client={client}>
+        <Query query={allAdvantages}>
+            {({loading, error, data}) => {
+                if (loading) return "Loading...";
+                if (error) return `Error! ${error.message}`;
 
-            //const random_index =  Math.floor(Math.random() * data.getQuotes.length);
-            //const quote = data.getQuotes[random_index];
-            const advantages = data.allAdvantages;
+                //const random_index =  Math.floor(Math.random() * data.getQuotes.length);
+                //const quote = data.getQuotes[random_index];
+                const advantages = data.allAdvantages;
 
-            return (
-                <ul>
-                    {advantages.map((a) => <li>
-                            {a.name} ({a.cost.join()})
-                        {(a.source_books || []).join()}
-                    </li>)}
-                </ul>
-            );
-        }}
-    </Query>
-</ApolloProvider>;
+                return (
+                    <List>
+                        {advantages.map((a,i) => <ListItem
+                            key={`allAdvantages_${i}`}
+                            primaryText={a.name}
+                            secondaryText={`Cost: ${a.cost.join('/')} | Source: ${(a.source_books || ['-']).join(',')}`}
+                        />)}
+                    </List>
+                );
+            }}
+        </Query>
+    </ApolloProvider>
+</MuiThemeProvider>;
 
 export default App;
